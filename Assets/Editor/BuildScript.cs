3209b4c9ff46bb7eba9f3227e184efff5416f9a1
matt <matt@matthewmason.ca>
2017-11-25 20:01:55 -0400

    Build options

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class AutomatedBuild : MonoBehaviour
+{
+    public static void BuildWebGLTest()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = new string[] {
+            "Assets/main.unity"
+        };
+
+        options.targetGroup = BuildTargetGroup.WebGL;
+        options.target = BuildTarget.WebGL;
+        options.locationPathName = "Build/Testing/WebGL";
+
+        BuildPipeline.BuildPlayer(options);
+    }
+
+    public static void BuildWebGL()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = new string[] {
+            "Assets/main.unity"
+        };
+
+        options.targetGroup = BuildTargetGroup.WebGL;
+        options.target = BuildTarget.WebGL;
+        options.locationPathName = "Build/Jam/WebGL";
+
+        BuildPipeline.BuildPlayer(options);
+    }
+}
