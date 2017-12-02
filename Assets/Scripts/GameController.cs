22b9044b8e1eb6efda055db86bd4a50c8badb876
Desktop\Richard <richard.sage28@gmail.com>
2017-12-02 12:04:43 -0400

    Added game controller script and prefab object

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private int score;
+    private int numOfPassengers;
+
+    public int Score {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public int NumOfPassengers {
+        get { return numOfPassengers; }
+        set { numOfPassengers = value; }
+    }
+
+    // Use this for initialization
+    void Start () {
+        Score = 0;
+        NumOfPassengers = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
