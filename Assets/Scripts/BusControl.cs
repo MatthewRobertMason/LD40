796090b68538fc43f2b13f595b35f4a8cf398b1a
matt <matt@matthewmason.ca>
2017-12-02 12:05:20 -0400

    folder corrections and some objects added

--- Assets/Scripts/BusControl.cs
+++ Assets/Scripts/BusControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dummy : MonoBehaviour {
+public class BusControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
